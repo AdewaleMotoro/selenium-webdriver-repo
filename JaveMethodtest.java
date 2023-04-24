3b57c6fa1ceb704887d7cb4694c45ec67fe47b9c
AdeMotoro <93820103+AdewaleMotoro@users.noreply.github.com>
2023-04-24 06:24:40 +0100

    Add files via upload

---
+++ JaveMethodtest.java
@@ -0,0 +1,15 @@
+public class JaveMethodtest {
+
+    public static void main(String[] args){
+
+     JaveMethodtest game = new JaveMethodtest();
+     game.getData();
+
+    }
+
+    //Always put method outside the clas or main
+    public void getData() {
+        System.out.println("Havesters");
+    }
+
+}
